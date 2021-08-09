45ce531f161c1c7a6bb74d8ffe8288b4f9dfefb8
Axel Lois <axel9034@hotmail.com>
2021-08-09 20:23:23 -0300

    login pronto

--- js/login.js
+++ js/login.js
@@ -9,25 +9,18 @@ document.addEventListener("DOMContentLoaded", function(e){
 const email = document.getElementById("email");
 const pass = document.getElementById("password");
 const form = document.getElementById("form");
-const parrafo = document.getElementById("warnings");
 
 form.addEventListener("submit",e=> {
     e.preventDefault();
-    let warnings = "";
-    let entry = false;
     let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
-    parrafo.innerHTML= "";
     if (!regexEmail.test(email.value)) {
         alert("El email no es valido");
         warnings+= `El email no es valido <br>`;
-        entry = true;
     }
     if (pass.value.length < 6) {
         alert("La contraseña es muy corta");
-        warnings+= `La contraseña es muy corta <br>`;
-        entry=true;
     }
-    if (entry) {
-        parrafo.innerHTML = warnings;
+    else {
+        location.href= "index.html";
     }
 })
